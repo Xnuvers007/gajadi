f2b6c510ee12ae91444a0de84ef359cad4bc359f
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2021-05-15 14:10:41 +0700

    Add files via upload

---
+++ helixdata/sidebar.php
@@ -0,0 +1,182 @@
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                    <i class="fas fa-code"></i>
+                <div class="sidebar-brand-text mx-3">Tools Online V.1</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Tools Online V.1
+            </div>
+
+            <!-- Nav Item - Defacer Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-file-code"></i>
+                    <span>Defacer Tools</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tools Online</h6>
+                        <a class="collapse-item" href="adfinder.php">Admin Finder</a>
+                        <a class="collapse-item" href="autovisitorblog.php">Auto Visitor Blog</a>
+                        <a class="collapse-item" href="autosqlbalitbang.php">Auto Sql Balitbang</a>
+                        <a class="collapse-item" href="crackpasswordbalitbang.php">Crack Sql Balitbang</a>
+                        <a class="collapse-item" href="clickjacking.php">Click Jacking</a>
+                        <a class="collapse-item" href="csrf.php">CSRF</a>
+                         <a class="collapse-item" href="cbtscan.php">CBT Exploit Scanner</a>
+                        <a class="collapse-item" href="generator.php">Hash Generator</a>
+                        <a class="collapse-item" href="identifier.php">Hash Identifier</a>
+                        <a class="collapse-item" href="jso.php">JSO Auto Create</a>
+                        <a class="collapse-item" href="ojs.php">OJS Shell Finder</a>
+                        <a class="collapse-item" href="sqli.php">SQL Injection Lib</a>
+                        <a class="collapse-item" href="scriptgenerator.php">Script Deface</a>
+                        <a class="collapse-item" href="shellchecker.php">Shell Checker</a>
+                        <a class="collapse-item" href="zonh.php">Zone-H Notifier</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Programmer Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProgram"
+                    aria-expanded="true" aria-controls="collapseProgram">
+                    <i class="fas fa-laptop"></i>
+                    <span>Programmer Tools</span>
+                </a>
+                <div id="collapseProgram" class="collapse" aria-labelledby="headingProgram"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tools Online</h6>
+                        <a class="collapse-item" href="base32.php">Base32</a>
+                        <a class="collapse-item" href="base64.php">Base64</a>
+                        <a class="collapse-item" href="bib.php">Bib Cipher</a>
+                        <a class="collapse-item" href="cipher.php">Caesar Cipher</a>
+                        <a class="collapse-item" href="enc-dec.php">Encode-Decode</a>
+                        <a class="collapse-item" href="hex.php">Hex</a>
+                        <a class="collapse-item" href="live.php">Live Coding</a>
+                        <a class="collapse-item" href="octal.php">Octal</a>
+                        <a class="collapse-item" href="obfuscate.php">PHP Obfuscator</a>
+                        <a class="collapse-item" href="vig.php">Vigenere Cipher</a>
+                        <a class="collapse-item" href="quaternary.php">Quaternary</a>
+                    </div>
+                </div>
+            </li>
+            
+             <!-- Nav Item - Network Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseNetwork"
+                    aria-expanded="true" aria-controls="collapseNetwork">
+                    <i class="fas fa-server"></i>
+                    <span>Networks Tools</span>
+                </a>
+                <div id="collapseNetwork" class="collapse" aria-labelledby="headingNetwork"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tools Online</h6>
+                        <a class="collapse-item" href="checkip.php">Domain IP Checker</a>
+                        <a class="collapse-item" href="domain.php">Domain Checker</a>
+                        <a class="collapse-item" href="ip.php">Mass Reverse Domain To Ip</a>
+                        <a class="collapse-item" href="revip.php">Reverse IP</a>
+                        <a class="collapse-item" href="subdofin.php">Subdomain Finder V.1</a>
+                        <a class="collapse-item" href="subdo.php">Subdomain Finder V.2</a>
+                        <a class="collapse-item" href="whois.php">Whois LookUp</a>
+                    </div>
+                </div>
+            </li>
+            
+             <!-- Nav Item - Checker Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseChecker"
+                    aria-expanded="true" aria-controls="collapseChecker">
+                    <i class="fas fa-search"></i>
+                    <span>Checkers Tools</span>
+                </a>
+                <div id="collapseChecker" class="collapse" aria-labelledby="headingChecker"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tools Online</h6>
+                        <a class="collapse-item" href="amazon.php">Amazon Checker</a>
+                    </div>
+                </div>
+            </li>
+            
+            
+             <!-- Nav Item - Games Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseGames"
+                    aria-expanded="true" aria-controls="collapseGames">
+                    <i class="fas fa-globe"></i>
+                    <span>Games Tools</span>
+                </a>
+                <div id="collapseGames" class="collapse" aria-labelledby="headingGames"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tools Online</h6>
+                        <a class="collapse-item" href="car.php">Car</a>
+                        <a class="collapse-item" href="cat.php">Cat Runner</a>
+                        <a class="collapse-item" href="catur.php">Catur Chess</a>
+                        <a class="collapse-item" href="dino.php">Dino Runner Automatic</a>
+                        <a class="collapse-item" href="fingerprint.php">Fingerprint</a>
+                        <a class="collapse-item" href="flappy-parrot.php">Flappy Parrot</a>
+                        <a class="collapse-item" href="jetshoot.php">Jet Shoot</a>
+                        <a class="collapse-item" href="ping-pong.php">Ping-Pong</a>
+                        <a class="collapse-item" href="shuffle.php">Shuffle</a>
+                        <a class="collapse-item" href="snake.php">Snake</a>
+                        <a class="collapse-item" href="https://play.anympedia.my.id">Other Game</a>
+                    </div>
+                </div>
+            </li>
+            
+             <!-- Nav Item - Others Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOthers"
+                    aria-expanded="true" aria-controls="collapseOthers">
+                    <i class="fas fa-folder-open"></i>
+                    <span>Others Tools</span>
+                </a>
+                <div id="collapseOthers" class="collapse" aria-labelledby="headingOthers"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Tools Online</h6>
+                        <a class="collapse-item" href="calculator.php">Calculator</a>
+                        <a class="collapse-item" href="/weton-master">Cek Jodoh</a>
+                        <a class="collapse-item" href="/Ngaji-Online">Ngaji Online</a>
+                        <a class="collapse-item" href="live.php">Live Coding</a>
+                        <a class="collapse-item" href="privacy.php">Privacy Generator (Indonesian)</a>
+                        <a class="collapse-item" href="disclaim.php">Disclaimer Generator (Indonesian</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
